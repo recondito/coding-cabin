368ac6b12bbdd16b102cbe54e2e472735d102808
Recondito <santanallanes@hotmail.com>
2024-12-04 09:46:44 -0400

    Index moved to Nunjucks. Tested and working.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,9 @@
 module.exports = function (eleventyConfig) {
     markdownTemplateEngine: "njk";
+    eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/fonts');
+    eleventyConfig.addPassthroughCopy('./src/img');
+    eleventyConfig.addPassthroughCopy('./src/js');
     return {
       dir: {
         input: "src",
